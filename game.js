8cd91a802a151f228bb6f5a394383e503535108c
Josh <zylo.codes@gmail.com>
2022-09-12 19:37:07 -0500

    pseudo-code

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-console.log('Yo!')
+// define array for choices [r, p, s]
+// define var for computerChoice
+// define var for humanChoice
+// define var for wins
+// define var for losses
+// define var for ties
+
+// prompt humanChoice
+
+// randomly choose computerChoice
+
+// compare choices
+
+// display (alert) comparison results (won, tied, lost round)
+
+// show stats (number of wins, losses, ties)
+
+// play again?
+  // restart game
+// else
+  // end game
