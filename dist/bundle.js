6538f014c6549a892e8ff1d983621d49871f48ad
Josh <zylo.codes@gmail.com>
2023-01-11 20:28:48 -0600

    Loaders and Plugins

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,113 @@
-(()=>{"use strict";var e={740:(e,n,r)=>{r.d(n,{Z:()=>s});var t=r(81),o=r.n(t),a=r(645),i=r.n(a)()(o());i.push([e.id,":root {\r\n  --oxfordBlue: #031D44;\r\n  --prussianBlue: #04395E;\r\n  --polishedPine: #70A288;\r\n  --burlywood: #dab785;\r\n  --copperCrayola: #D5896F;\r\n  --lavenderWeb: #DED9E2;\r\n}\r\n\r\n* {\r\n  vertical-align: baseline;\r\n  font-weight: inherit;\r\n  font-family: inherit;\r\n  font-style: inherit;\r\n  font-size: 100%;\r\n  border: 0 none;\r\n  outline: 0;\r\n  padding: 0;\r\n  margin: 0;\r\n}\r\nbody {\r\n  color: var(--lavenderWeb);\r\n  background-color: var(--prussianBlue);\r\n  padding: 5px;\r\n  font-size: 32px;\r\n  font-family: 'Rubik Dirt', cursive;\r\n}\r\nh1, h2, h3, h4, h5, h6, p, pre, blockquote, form, ul, ol, dl {\r\n  margin: 20px 0;\r\n}\r\nli, dd, blockquote {\r\n  margin-left: 40px;\r\n}\r\ntable {\r\n  border-collapse: collapse;\r\n  border-spacing: 0;\r\n}\r\n\r\nheader {\r\n  display: flex;\r\n  justify-content: space-between;\r\n  align-items: center;\r\n  padding: 0 30px;\r\n}\r\n\r\nh1 {\r\n  font-size: 52px;\r\n}\r\n\r\n#choices-container {\r\n  display: flex;\r\n  justify-content: space-around;\r\n  margin: 120px auto 0;\r\n  max-width: 1280px;\r\n}\r\n\r\n.choice {\r\n  cursor: pointer;\r\n  transition: transform 260ms ease-out;\r\n  text-align: center;\r\n}\r\n\r\n.choice:hover {\r\n  transform: scale(1.2);\r\n}\r\n\r\n.choice img {\r\n  display: block;\r\n}\r\n\r\n.loser {\r\n  opacity: 0.45;\r\n}",""]);const s=i},645:e=>{e.exports=function(e){var n=[];return n.toString=function(){return this.map((function(n){var r="",t=void 0!==n[5];return n[4]&&(r+="@supports (".concat(n[4],") {")),n[2]&&(r+="@media ".concat(n[2]," {")),t&&(r+="@layer".concat(n[5].length>0?" ".concat(n[5]):""," {")),r+=e(n),t&&(r+="}"),n[2]&&(r+="}"),n[4]&&(r+="}"),r})).join("")},n.i=function(e,r,t,o,a){"string"==typeof e&&(e=[[null,e,void 0]]);var i={};if(t)for(var s=0;s<this.length;s++){var c=this[s][0];null!=c&&(i[c]=!0)}for(var l=0;l<e.length;l++){var d=[].concat(e[l]);t&&i[d[0]]||(void 0!==a&&(void 0===d[5]||(d[1]="@layer".concat(d[5].length>0?" ".concat(d[5]):""," {").concat(d[1],"}")),d[5]=a),r&&(d[2]?(d[1]="@media ".concat(d[2]," {").concat(d[1],"}"),d[2]=r):d[2]=r),o&&(d[4]?(d[1]="@supports (".concat(d[4],") {").concat(d[1],"}"),d[4]=o):d[4]="".concat(o)),n.push(d))}},n}},81:e=>{e.exports=function(e){return e[1]}},379:e=>{var n=[];function r(e){for(var r=-1,t=0;t<n.length;t++)if(n[t].identifier===e){r=t;break}return r}function t(e,t){for(var a={},i=[],s=0;s<e.length;s++){var c=e[s],l=t.base?c[0]+t.base:c[0],d=a[l]||0,u="".concat(l," ").concat(d);a[l]=d+1;var p=r(u),f={css:c[1],media:c[2],sourceMap:c[3],supports:c[4],layer:c[5]};if(-1!==p)n[p].references++,n[p].updater(f);else{var m=o(f,t);t.byIndex=s,n.splice(s,0,{identifier:u,updater:m,references:1})}i.push(u)}return i}function o(e,n){var r=n.domAPI(n);return r.update(e),function(n){if(n){if(n.css===e.css&&n.media===e.media&&n.sourceMap===e.sourceMap&&n.supports===e.supports&&n.layer===e.layer)return;r.update(e=n)}else r.remove()}}e.exports=function(e,o){var a=t(e=e||[],o=o||{});return function(e){e=e||[];for(var i=0;i<a.length;i++){var s=r(a[i]);n[s].references--}for(var c=t(e,o),l=0;l<a.length;l++){var d=r(a[l]);0===n[d].references&&(n[d].updater(),n.splice(d,1))}a=c}}},569:e=>{var n={};e.exports=function(e,r){var t=function(e){if(void 0===n[e]){var r=document.querySelector(e);if(window.HTMLIFrameElement&&r instanceof window.HTMLIFrameElement)try{r=r.contentDocument.head}catch(e){r=null}n[e]=r}return n[e]}(e);if(!t)throw new Error("Couldn't find a style target. This probably means that the value for the 'insert' parameter is invalid.");t.appendChild(r)}},216:e=>{e.exports=function(e){var n=document.createElement("style");return e.setAttributes(n,e.attributes),e.insert(n,e.options),n}},565:(e,n,r)=>{e.exports=function(e){var n=r.nc;n&&e.setAttribute("nonce",n)}},795:e=>{e.exports=function(e){var n=e.insertStyleElement(e);return{update:function(r){!function(e,n,r){var t="";r.supports&&(t+="@supports (".concat(r.supports,") {")),r.media&&(t+="@media ".concat(r.media," {"));var o=void 0!==r.layer;o&&(t+="@layer".concat(r.layer.length>0?" ".concat(r.layer):""," {")),t+=r.css,o&&(t+="}"),r.media&&(t+="}"),r.supports&&(t+="}");var a=r.sourceMap;a&&"undefined"!=typeof btoa&&(t+="\n/*# sourceMappingURL=data:application/json;base64,".concat(btoa(unescape(encodeURIComponent(JSON.stringify(a))))," */")),n.styleTagTransform(t,e,n.options)}(n,e,r)},remove:function(){!function(e){if(null===e.parentNode)return!1;e.parentNode.removeChild(e)}(n)}}}},589:e=>{e.exports=function(e,n){if(n.styleSheet)n.styleSheet.cssText=e;else{for(;n.firstChild;)n.removeChild(n.firstChild);n.appendChild(document.createTextNode(e))}}}},n={};function r(t){var o=n[t];if(void 0!==o)return o.exports;var a=n[t]={id:t,exports:{}};return e[t](a,a.exports,r),a.exports}r.n=e=>{var n=e&&e.__esModule?()=>e.default:()=>e;return r.d(n,{a:n}),n},r.d=(e,n)=>{for(var t in n)r.o(n,t)&&!r.o(e,t)&&Object.defineProperty(e,t,{enumerable:!0,get:n[t]})},r.o=(e,n)=>Object.prototype.hasOwnProperty.call(e,n),r.nc=void 0,(()=>{var e=r(379),n=r.n(e),t=r(795),o=r.n(t),a=r(569),i=r.n(a),s=r(565),c=r.n(s),l=r(216),d=r.n(l),u=r(589),p=r.n(u),f=r(740),m={};m.styleTagTransform=p(),m.setAttributes=c(),m.insert=i().bind(null,"head"),m.domAPI=o(),m.insertStyleElement=d(),n()(f.Z,m),f.Z&&f.Z.locals&&f.Z.locals;const v=document.getElementById("human-score"),h=document.getElementById("bot-score"),y=document.getElementById("rock"),g=document.getElementById("paper"),b=document.getElementById("scissors"),x=document.getElementById("result");var E=[y,g,b],T=["r","p","s"],k=0,I=0,B=!0;function O(e){if(B){var n,r,t=(n=e.target.matches("img")?e.target.parentElement:e.target).dataset.letter,o=Math.floor(Math.random()*T.length),a=T[o],i=document.querySelector('[data-letter="'+a+'"]');t===a?r="TIED!":"r"===t&&"s"===a||"p"===t&&"r"===a||"s"===t&&"p"===a?(k++,r="YOU WON!"):(I++,r="BOT WON!"),function(e,n,r){B=!1,x.innerText=r,v.innerText=k,h.innerText=I,E.forEach((function(e){e.style.display="none"})),e.style.display="block",n.style.display="block";var t=document.createElement("span");t.innerText="You",e.appendChild(t);var o=document.createElement("span");o.innerText="Bot",n.appendChild(o),"YOU WON!"===r?n.classList.add("loser"):"BOT WON!"===r&&e.classList.add("loser"),setTimeout((function(){B=!0,x.innerText="Rock Paper Scissors",E.forEach((function(e){e.style.display="block",e.classList.remove("loser");for(var n=e.querySelectorAll("span"),r=0;r<n.length;r++)n[r].remove()}))}),3e3)}(n,i,r)}}y.addEventListener("click",O),g.addEventListener("click",O),b.addEventListener("click",O)})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/elements.js
+const humanScoreEl = document.getElementById('human-score')
+const botScoreEl = document.getElementById('bot-score')
+const rockEl = document.getElementById('rock')
+const paperEl = document.getElementById('paper')
+const scissorsEl = document.getElementById('scissors')
+const resultEl = document.getElementById('result')
+
+;// CONCATENATED MODULE: ./src/js/main.js
+
+
+
+
+var choicesElements = [rockEl, paperEl, scissorsEl]
+var choices = ["r", "p", "s"]
+var humanScore = 0
+var botScore = 0
+var clickable = true 
+
+function resetUI() {
+  clickable = true
+  resultEl.innerText = "Rock Paper Scissors"
+  // show all three choices
+  choicesElements.forEach(function(el) {
+    el.style.display = 'block'
+    el.classList.remove('loser')
+    var spans = el.querySelectorAll('span')
+    for (var i = 0; i < spans.length; i++) {
+      spans[i].remove()
+    }
+  })
+}
+
+function updateScores() {
+  humanScoreEl.innerText = humanScore
+  botScoreEl.innerText = botScore
+}
+
+function showResults(humanChosenEl, computerChosenEl, result) {
+  clickable = false
+  resultEl.innerText = result
+  updateScores()
+  // hide all three choice
+  choicesElements.forEach(function(el) {
+    el.style.display = 'none'
+  })
+  humanChosenEl.style.display = "block"
+  computerChosenEl.style.display = "block"
+
+  var humanSpan = document.createElement('span')
+  humanSpan.innerText = "You"
+  humanChosenEl.appendChild(humanSpan)
+
+  var botSpan = document.createElement('span')
+  botSpan.innerText = "Bot"
+  computerChosenEl.appendChild(botSpan)
+
+  if (result === 'YOU WON!') {
+    computerChosenEl.classList.add('loser')
+  } else if (result === 'BOT WON!') {
+    humanChosenEl.classList.add('loser')
+  }
+
+  setTimeout(function() {
+    resetUI()
+  }, 3000)
+}
+
+function startRound(event) {
+  if (!clickable) return
+
+  var humanChosenEl
+  // make sure we're referring to the correct element
+  if (event.target.matches('img')) {
+    humanChosenEl = event.target.parentElement
+  } else {
+    humanChosenEl = event.target
+  }
+
+  var humanChoice = humanChosenEl.dataset.letter
+  
+  // randomly choose computerChoice
+  var random = Math.floor(Math.random() * choices.length)
+  var computerChoice = choices[random]
+  var computerChosenEl = document.querySelector('[data-letter="'+ computerChoice +'"]')
+
+  var result
+  if (humanChoice === computerChoice) {
+    result = "TIED!"
+  } else if (
+    humanChoice === "r" && computerChoice === "s" ||
+    humanChoice === "p" && computerChoice === "r" ||
+    humanChoice === "s" && computerChoice === "p"
+  ) {
+    humanScore++
+    result = "YOU WON!"
+  } else {
+    botScore++
+    result = "BOT WON!"
+  }
+
+  showResults(humanChosenEl, computerChosenEl, result)
+}
+
+rockEl.addEventListener('click', startRound)
+paperEl.addEventListener('click', startRound)
+scissorsEl.addEventListener('click', startRound)
+/******/ })()
+;
